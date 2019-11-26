1d4effceefc3bb79f24dfcd705433c51938c133b
Pougliche <pugliese.viktor@gmail.com>
2019-11-26 15:50:09 +0100

    ajout user_controlleur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController 
   has_one_attached :avatar
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user
+  before_action :my_page, only: [:show]
+ 
+  def index
+    @users = User.all
+  end
+
 
   def show
     @user = User.find(params[:id])
