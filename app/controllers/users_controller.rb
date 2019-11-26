af8246ad5d999ad194cb0ada16620407d44cc2e5
Pougliche <pugliese.viktor@gmail.com>
2019-11-26 17:26:35 +0100

    yo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,57 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+      if @user.save
+        redirect_to @user, notice: 'User was successfully created.'
+        
+      else
+        render :new 
+        
+      end
+    
+  end
+
+
+  def update
+    
+      if @user.update(user_params)
+       redirect_to @user, notice: 'User was successfully updated.'
+      else
+       render :edit 
+        
+      end
+    
+  end
+
+  def destroy
+    @user.destroy
+      redirect_to users_url, notice: 'User was successfully destroyed.' 
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    return params.require(:user).permit(:first_name, :last_name, :user_name, :email)
+  end
+
+  def my_page
+    unless current_user == User.find(params[:id])
+      redirect_to items_path, flash: {danger: "Acces restreint !" }
+    end
+  end
 end
