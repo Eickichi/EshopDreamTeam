fcd287cdc73bafd9a1ae9f527624551df34bb3e5
Eickichi <Davidye75019@gmail.com>
2019-11-26 15:21:34 +0100

    Starting active storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+  has_one_attached :avatar
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :orders
   resources :carts
   resources :products
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
