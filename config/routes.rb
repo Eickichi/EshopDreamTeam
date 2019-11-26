04f521542ffdb81406e3de280938a311090cbd01
Pougliche <pugliese.viktor@gmail.com>
2019-11-26 15:11:39 +0100

    ajout de tous les controleurs + cart helper + une modif route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :carts
   resources :products
+  post 'line_items' => "line_items#create"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
